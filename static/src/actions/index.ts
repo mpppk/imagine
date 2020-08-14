bfb27e8f165ed04a71101afdfe343f6e63092411
niboshi <niboshiporipori@gmail.com>
2020-08-14 22:55:51 +0900

    Manage tags on redux state (#153)

--- static/src/actions/index.ts
+++ static/src/actions/index.ts
@@ -1,11 +1,14 @@
 import actionCreatorFactory from 'typescript-fsa';
 import {WSPayload} from "./server";
+import {Tag} from "../models/models";
 
 const indexActionCreatorFactory = actionCreatorFactory('INDEX');
 
 export const indexActionCreators = {
-  clickAddDirectoryButton: indexActionCreatorFactory<WSPayload>(
-    'CLICK_ADD_DIRECTORY_BUTTON'
-  ),
+  clickAddDirectoryButton: indexActionCreatorFactory<WSPayload>('CLICK_ADD_DIRECTORY_BUTTON'),
+  clickAddTagButton: indexActionCreatorFactory<Tag>('CLICK_ADD_TAG_BUTTON'),
+  clickEditTagButton: indexActionCreatorFactory<Tag>('CLICK_EDIT_TAG_BUTTON'),
+  renameTag: indexActionCreatorFactory<Tag>('RENAME_TAG'),
+  updateTags: indexActionCreatorFactory<Tag[]>('UPDATE_TAGS'),
 };
 
--- static/src/reducers/global.ts
+++ static/src/reducers/global.ts
@@ -2,16 +2,28 @@ import {reducerWithInitialState} from 'typescript-fsa-reducers';
 import {assetActionCreators} from "../actions/asset";
 import {globalActionCreators} from '../actions/global';
 import {serverActionCreators} from "../actions/server";
-import {Asset, WorkSpace} from '../models/models';
+import {Asset, Tag, WorkSpace} from '../models/models';
+import {indexActionCreators} from "../actions";
+import {immutableSplice} from "../util";
+
+// fake data generator
+const generateTags = (count: number) =>
+  Array.from({length: count}, (_, k) => k).map(k => ({
+    id: k,
+    name: `item-${k}`,
+  } as Tag));
 
 export const globalInitialState = {
   assets: [] as Asset[],
+  tags: generateTags(5) as Tag[], // FIXME
+  // maxTagId: 10,
   currentWorkSpace: null as WorkSpace | null,
   hasMoreAssets :true,
   isLoadingWorkSpaces: true,
   isScanningAssets: false,
   workspaces: null as WorkSpace[] | null,
 };
+
 
 export type GlobalState = typeof globalInitialState;
 export const global = reducerWithInitialState(globalInitialState)
@@ -30,4 +42,21 @@ export const global = reducerWithInitialState(globalInitialState)
   })
   .case(globalActionCreators.selectNewWorkSpace, (state, workspace) => {
     return {...state, currentWorkSpace: workspace};
+  })
+  .case(globalActionCreators.selectNewWorkSpace, (state, workspace) => {
+    return {...state, currentWorkSpace: workspace};
+  })
+  .case(indexActionCreators.clickAddTagButton, (state, tag) => {
+    return {...state, tags: [tag, ...state.tags]};
+  })
+  .case(indexActionCreators.renameTag, (state, tag) => {
+    const targetTagIndex = state.tags.findIndex((t) => t.id === tag.id);
+    if (targetTagIndex === -1) {
+      // tslint:disable-next-line:no-console
+      console.warn('unknown tag ID is provided', tag);
+    }
+    return {...state, tags: immutableSplice(state.tags, targetTagIndex, 1, tag)};
+  })
+  .case(indexActionCreators.updateTags, (state, tags) => {
+    return {...state, tags};
   })
