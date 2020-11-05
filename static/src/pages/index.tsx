94b0fc062207f2a7fa2f2a1dbff9f9c8e0bca805
niboshi <niboshiporipori@gmail.com>
2020-11-05 13:28:52 +0900

    Add AssetInfoTable component (#268)

--- static/src/pages/index.tsx
+++ static/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import {Button, Typography} from "@material-ui/core";
+import {Button} from "@material-ui/core";
 import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import React, {useState} from 'react';
@@ -17,6 +17,7 @@ import uniq from "lodash/uniq";
 import _ from "lodash";
 import {boundingBoxActionCreators} from "../actions/box";
 import {Pixel} from "../components/svg/svg";
+import {AssetInfoTable} from "../components/AssetInfoTable";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -210,18 +211,7 @@ export default function Test() {
           onScaleBoundingBox={handlers.onScaleBoundingBox}
           onDeleteBoundingBox={handlers.onDeleteBoundingBox}
         />}
-        <Typography paragraph={true}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-          ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-          facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-          gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-          donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-          Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-          imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-          arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-          donec massa sapien faucibus et molestie ac.
-        </Typography>
+        {globalState.selectedAsset ? <AssetInfoTable asset={globalState.selectedAsset}/> : null}
         <Button variant="outlined" color="primary"
                 disabled={globalState.isScanningDirectories || globalState.isLoadingWorkSpace}
                 onClick={handleClickAddDirectoryButton}>
