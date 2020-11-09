77531f96a8c0e88855183e5a81385065f105b531
niboshi <niboshiporipori@gmail.com>
2020-11-09 14:09:46 +0900

    Add no-tags and start-with query (#280)
    * Add start-with and no-tags query to dialog
    
    * Implement query filtering

--- domain/model/query.go
+++ domain/model/query.go
@@ -1,25 +1,33 @@
 package model
 
-import "log"
+import (
+	"log"
+)
 
 type QueryOP string
 
 const (
 	EqualsQueryOP    QueryOP = "equals"
 	NotEqualsQueryOP QueryOP = "not-equals"
+	StartWithQueryOP QueryOP = "start-with"
+	NoTagsQueryOP    QueryOP = "no-tags"
 )
 
 type Query struct {
-	Op      QueryOP `json:"op"`
-	TagName string  `json:"tagName"`
+	Op    QueryOP `json:"op"`
+	Value string  `json:"value"`
 }
 
 func (q *Query) Match(asset *Asset) bool {
 	switch q.Op {
 	case EqualsQueryOP:
-		return asset.HasTag(q.TagName)
+		return asset.HasTag(q.Value)
 	case NotEqualsQueryOP:
-		return !asset.HasTag(q.TagName)
+		return !asset.HasTag(q.Value)
+	case StartWithQueryOP:
+		return asset.HasTagStartWith(q.Value)
+	case NoTagsQueryOP:
+		return len(asset.BoundingBoxes) == 0
 	default:
 		log.Printf("warning: unknown query op is given: %s", q.Op)
 		return false
