2da783799a9365b129a8a1efadd26e1645126c97
mpppk <niboshiporipori@gmail.com>
2020-11-17 10:09:17 +0900

    Remove explorer link from side list

--- static/src/components/drawer/SideList.tsx
+++ static/src/components/drawer/SideList.tsx
@@ -28,14 +28,6 @@ const SideList: React.FunctionComponent = () => {
             <ListItemText primary={'Home'} />
           </ListItem>
         </Link>
-        <Link href={'/explorer'}>
-          <ListItem button={true} key={'Explorer'}>
-            <ListItemIcon>
-              <InfoIcon />
-            </ListItemIcon>
-            <ListItemText primary={'Explorer'} />
-          </ListItem>
-        </Link>
       </List>
     </div>
   );
